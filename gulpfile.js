28e265ac0a14f728b8c799b7c42af1654679e23d
Stephen Colacurcio <stephencolacurcio@gmail.com>
2015-11-09 16:02:24 -0500

    added autoprefixer to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 var gulp = require('gulp'),
-	 uglify = require('gulp-uglify'),
-	 sass = require('gulp-sass'),
-	 eslint = require('gulp-eslint'),
-	 rename = require('gulp-rename'),
-	 livereload = require('gulp-livereload'),
-	 plumber = require('gulp-plumber'),
-	 sourcemaps = require('gulp-sourcemaps'),
-	 runSeq = require('run-sequence'),
-	 concat = require('gulp-concat'),
-	 babel = require('gulp-babel');
+	uglify = require('gulp-uglify'),
+	sass = require('gulp-sass'),
+	eslint = require('gulp-eslint'),
+	rename = require('gulp-rename'),
+	livereload = require('gulp-livereload'),
+	plumber = require('gulp-plumber'),
+	sourcemaps = require('gulp-sourcemaps'),
+	runSeq = require('run-sequence'),
+	concat = require('gulp-concat'),
+	babel = require('gulp-babel'),
+	autoprefixer = require('gulp-autoprefixer');
 
 /*Live reload task*/
 gulp.task('reload', function () {
@@ -20,20 +21,21 @@ gulp.task('reloadCSS', function () {
 });
 
 gulp.task('buildCSS', function () {
-    return gulp.src('./browser/scss/**/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename('style.css'))
-        .pipe(gulp.dest('./public'));
+	return gulp.src('./browser/scss/**/*.scss')
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(autoprefixer('last 2 version'))
+		.pipe(rename('style.css'))
+		.pipe(gulp.dest('./public'));
 });
 
 gulp.task('buildJS', ['lintJS'], function () {
-    return gulp.src(['./browser/js/app.js', './browser/js/**/*.js'])
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(concat('main.js'))
-        .pipe(babel({presets: ['es2015']}))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./public'));
+	return gulp.src(['./browser/js/app.js', './browser/js/**/*.js'])
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
+		.pipe(concat('main.js'))
+		.pipe(babel({presets: ['es2015']}))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./public'));
 });
 
 gulp.task('lintJS', function () {
