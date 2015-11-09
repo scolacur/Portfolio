bc437bbc3f309e013676ae989bf69cf7273457a1
Stephen Colacurcio <stephencolacurcio@gmail.com>
2015-11-08 19:11:07 -0500

    initialized express app

---
+++ app.js
@@ -0,0 +1,14 @@
+//Main Application
+var express = require('express');
+var app = express();
+
+app.get('/', function(req,res){
+	res.send('Yoooo');
+});
+
+
+var server  = app.listen(2000, function(){
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('Example app listening at http://%s:%s', host, port);
+});
