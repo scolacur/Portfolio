675f4101b7835ef5e6afe8acb3735554c5f2b24c
Stephen Colacurcio <stephencolacurcio@gmail.com>
2015-11-10 22:19:13 -0500

    more silly design stuff

--- browser/js/about.js
+++ browser/js/about.js
@@ -1,16 +1,20 @@
 app.controller('aboutCtrl', function($scope){
 	var increment = 0;
 	var carousel = $('#carousel');
-
+	var current = '#fig-1';
+	var prev;
 	setInterval( function(){
 		increment++;
 		carousel.css({
 			'-webkit-transform': 'rotateX(' + (increment * -60) + 'deg)'
 		});
-		var pastSix = -1;
-		if(increment >= 6){
-			increment = 0;
-		}
-		carousel.attr('data-state',(increment % 6)+1);
+		carousel.attr('data-state',(increment % 6) + 1);
+
+		prev = current;
+		current = '#fig-' +  + ((increment % 6) + 1);
+		console.log(current);
+		$(current).css({'border-radius' : '30px'});
+		$(prev).css({'border-radius' : '0px'});
+
 	},1500);
 });
