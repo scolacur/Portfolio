073bffbeb935363e524ad733aa1abd66f2aef961
Stephen Colacurcio <stephencolacurcio@gmail.com>
2015-11-18 15:02:44 -0500

    finished home page

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -6,5 +6,5 @@ router.use('/contact', require('./contact'));
 // Make sure this is after all of
 // the registered routes!
 router.use(function (req, res) {
-    res.status(404).end();
+	res.status(404).end();
 });
