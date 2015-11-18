61648bb2b264c00c30ec0498bb23e504f86f676e
Stephen Colacurcio <stephencolacurcio@gmail.com>
2015-11-18 15:58:35 -0500

    added progress bar to scroll page but hid it

--- browser/js/project.js
+++ browser/js/project.js
@@ -6,14 +6,13 @@ app.controller('projectCtrl', function($scope){
 		var animTime = 1000;
 		var scrolling = false;
 		var pgPrefix = ".skw-page-";
+		var progPrefix = "#p";
 
 		function pagination() {
 			scrolling = true;
 
 			$(pgPrefix + curPage).removeClass("inactive").addClass("active");
-			$(pgPrefix + (curPage - 1)).addClass("inactive");
-			$(pgPrefix + (curPage + 1)).removeClass("active");
-
+			$(progPrefix + curPage).addClass("current");
 			setTimeout(function() {
 				scrolling = false;
 			}, animTime);
@@ -22,18 +21,23 @@ app.controller('projectCtrl', function($scope){
 		function navigateUp() {
 			if (curPage === 1) return;
 			curPage--;
+			$(pgPrefix + (curPage + 1)).removeClass("active");
+			$(progPrefix + (curPage + 1)).removeClass("current");
+
 			pagination();
 		}
 
 		function navigateDown() {
 			if (curPage === numOfPages) return;
 			curPage++;
+			$(pgPrefix + (curPage - 1)).removeClass("active");
+			$(progPrefix + (curPage -1)).removeClass("current");
 			pagination();
 		}
 
 		$(document).on("mousewheel DOMMouseScroll", function(e) {
 			if (scrolling) return;
-			if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+			if (e.originalEvent.wheelDelta > 0  || e.originalEvent.detail < 0) {
 				navigateUp();
 			} else {
 				navigateDown();
@@ -48,11 +52,5 @@ app.controller('projectCtrl', function($scope){
 				navigateDown();
 			}
 		});
-
-		$('#down-arrow').click(function(){
-			if (scrolling) return;
-			navigateDown();
-		});
-
 	});
 });
