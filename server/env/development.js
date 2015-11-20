696b2d88663017694484b462b9a13dd66a316a11
Stephen Colacurcio <stephencolacurcio@gmail.com>
2015-11-20 13:21:40 -0500

    ok now i think i configured mandrill

--- server/env/development.js
+++ server/env/development.js
@@ -1,7 +1,5 @@
 module.exports = {
   "SESSION_SECRET": "yolo",
-  "MANDRILL": {
-    "api": 'JUkg8-mc9CKIB3qcYubnLA'
-  }
+  "MANDRILL": 'JUkg8-mc9CKIB3qcYubnLA'
 };
   // "DATABASE_URI": "mongodb://localhost:27017/fsg-app",
