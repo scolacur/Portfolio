da001bcab970d2673e6bf24014f062e2bca5ebf4
Stephen Colacurcio <stephencolacurcio@gmail.com>
2015-11-20 16:48:56 -0500

    changed download link

--- server/env/development.js
+++ server/env/development.js
@@ -1,5 +1,4 @@
 module.exports = {
-  "SESSION_SECRET": "yolo",
-  "MANDRILL": 'JUkg8-mc9CKIB3qcYubnLA'
+	"SESSION_SECRET": "yolo",
+	"MANDRILL": 'JUkg8-mc9CKIB3qcYubnLA'
 };
-  // "DATABASE_URI": "mongodb://localhost:27017/fsg-app",
