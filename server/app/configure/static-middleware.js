045d2f04afda21135374f09de4b724003f72fe0b
Stephen Colacurcio <stephencolacurcio@gmail.com>
2015-12-05 19:46:46 -0500

    added compression

--- server/app/configure/static-middleware.js
+++ server/app/configure/static-middleware.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var express = require('express');
 var favicon = require('serve-favicon');
+var compression = require('compression');
 
 module.exports = function (app) {
 
@@ -11,6 +12,7 @@ module.exports = function (app) {
 	var publicPath = path.join(root, './public');
 	var browserPath = path.join(root, './browser');
 
+	app.use(compression());
 	app.use(favicon(app.getValue('faviconPath')));
 	app.use(express.static(npmPath));
 	app.use(express.static(bowerPath));
